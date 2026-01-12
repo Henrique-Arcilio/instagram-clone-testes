44c6f93b1972f8e155e46d89c0c7a88fae7c64e7
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2026-01-12 15:25:01 -0300

    test: add UserController.updateUser tests and @Transactional

--- backend/instagram/src/test/java/br/edu/ifpb/instagram/controller/UserControllerTest.java
+++ backend/instagram/src/test/java/br/edu/ifpb/instagram/controller/UserControllerTest.java
@@ -1,7 +1,10 @@
 package br.edu.ifpb.instagram.controller;
 
+import br.edu.ifpb.instagram.model.request.UserDetailsRequest;
 import br.edu.ifpb.instagram.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,11 +16,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc(addFilters = false)
+@Transactional
 class UserControllerTest {
 
     @Autowired
@@ -26,11 +31,15 @@ class UserControllerTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private ObjectMapper mapper;
+
     @Test
     void getUserById_ShouldReturnUserDetailsResponse() throws Exception {
 
         mockMvc.perform(get("/users/{id}", 1L))
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1L))
                 .andExpect(jsonPath("$.fullName").value("Test User1"))
                 .andExpect(jsonPath("$.username").value("testuser1"))
@@ -51,10 +60,47 @@ class UserControllerTest {
     @Test
     void getUsers_ShouldReturnAListOfUserDetailsResponse() throws Exception {
         mockMvc.perform(get("/users"))
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isArray())
                 .andExpect(jsonPath("$.size()").value(3))
                 .andExpect(jsonPath("$[*].username", containsInAnyOrder("testuser1", "testuser2", "testuser3")));
     }
 
+
+    @Test
+    void updateUser_ShouldReturnUserDetailsResponseWithUpdatedUserInfo() throws Exception {
+        UserDetailsRequest request = new UserDetailsRequest(
+                1L,
+                "updatedUser@gmail.com",
+                "updatedPassword",
+                "Updated User",
+                "updateUser");
+
+        mockMvc.perform(put("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.fullName").value("Updated User"))
+                .andExpect(jsonPath("$.username").value("updateUser"))
+                .andExpect(jsonPath("$.email").value("updatedUser@gmail.com"));
+    }
+
+    @Test
+    void updateUser_ThrowsExceptionWhenNullDataDTO() throws Exception {
+        UserDetailsRequest request = new UserDetailsRequest(
+                null,
+                "updatedUser@gmail.com",
+                "updatedPassword",
+                "Updated User",
+                "updateUser");
+
+        ServletException nullIdException = assertThrows(ServletException.class, () -> mockMvc.perform(put("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(request)))
+                .andReturn());
+
+        assertTrue(nullIdException.getMessage().contains("UserDto or UserDto.id must not be null"));
+    }
 }
