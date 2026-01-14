660754956a47cf32d6b44b9620e86e5a69d3e830
Yasmin Sarinho <yasmiinsarinho@gmail.com>
2026-01-13 22:57:04 -0300

    test: add validation and username extraction tests JwtUtils

--- backend/instagram/src/test/java/br/edu/ifpb/instagram/security/JwtUtilsTest.java
+++ backend/instagram/src/test/java/br/edu/ifpb/instagram/security/JwtUtilsTest.java
@@ -36,4 +36,23 @@ public class JwtUtilsTest {
         verify(auth, times(1)).getName();
     }
 
+    @Test // Objetivo: garantir que o token gerado é aceito como válido
+    void testValidateToken_ValidTokenReturnsTrue(){
+        when(auth.getName()).thenReturn("usuario_valido");
+        String token = jwtUtils.generateToken(auth);
+
+        boolean result = jwtUtils.validateToken(token);
+        assertTrue(result);
+    }
+
+    @Test
+    void testGetUsernameFromToken_ReturnsCorrectUsername(){
+        String expectedUser = "yasmin123";
+        when(auth.getName()).thenReturn(expectedUser);
+        String token = jwtUtils.generateToken(auth);
+
+        String resultUser = jwtUtils.getUsernameFromToken(token);
+        assertEquals(expectedUser, resultUser);
+    }
+
 }
